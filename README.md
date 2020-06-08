654bbd9e547a6af5e7411f375383b92da00bdc5b
Oscar Baracos <oscar.baracos@gmail.com>
2020-06-07 22:18:14 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# personal-website
-Repository for my personal website
+# Personal Website
+My personal website, built with Vue + Vuetify.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <v-row>
+          <v-col class="d-inline-flex">
+          <p class="display-4 font-weight-black red--text text--darken-4 mr-4">Oscar Baracos</p>
+          <v-btn fab icon x-large color="red darken-4" href="https://github.com/Oscbacon">
+            <v-icon x-large>fab fa-github</v-icon>
+          </v-btn>
+          <v-btn fab icon x-large color="red darken-4" href="https://www.linkedin.com/in/oscar-baracos-102ba4145/">
+            <v-icon x-large>fab fa-linkedin-in</v-icon>
+          </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
